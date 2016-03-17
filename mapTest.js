e74be1ee936105052c0a5d73ba5334f67ab82329
Dalton Nickerson <dnickers@unca.edu>
2016-03-17 11:39:07 -0400

    We have location based picture data!

--- mapTest.js
+++ mapTest.js
@@ -8,6 +8,33 @@
                 zoom: 18
             });
         },
+        Instagram = function (lat, lng) {
+            var access_token = "16384709.6ac06b4.49b97800d7fd4ac799a2c889f50f2587",                   locationIds=[];
+            $.ajax({
+                type: "GET",
+                dataType: "jsonp",
+                cache: false,
+                url: "https://api.instagram.com/v1/locations/search?lat=" + lat + "&lng=" + lng + "&access_token=" + access_token,
+                success: function (data) {
+                    for(var obj in data.data){
+                        locationIds.push(data.data[obj]);
+                    }
+                    for(var loc in locationIds){
+                        console.log("Checking location id:"+locationIds[loc].id+"\tname:"+locationIds[loc].name);
+                        $.ajax({
+                            type: "GET",
+                            dataType: "jsonp",
+                            cache: false,
+                            url: "https://api.instagram.com/v1/locations/"+locationIds[loc].id+"/media/recent?access_token="+access_token,
+                            success: function (data) {
+                                console.log(data);
+                            }
+                        });
+                    }
+                    //console.log(locationIds);
+                }
+            });
+        },
     
         main = function () {
             initMap();
@@ -18,6 +45,7 @@
                         lng: position.coords.longitude
                     };
                     map.setCenter(pos);
+                    Instagram(pos.lat,pos.lng);
                 });
             }
         };
