d26f4e876a7e7c2b57aece6bb137a515a7c4345f
Dalton Nickerson <dnickers@unca.edu>
2016-03-20 23:27:33 -0400

    Panning changes images.

--- mapTest.js
+++ mapTest.js
@@ -1,14 +1,42 @@
 (function () {
     "use strict";
     var map,
-
+        markers = [],
+        MAX_MARKERS = 200,
+        zoomRatios = {
+            20: 1128.497220,
+            19 : 2256.994440,
+            18 : 4513.988880,
+            17 : 9027.977761,
+            16 : 18055.955520,
+            15 : 36111.911040,
+            14 : 72223.822090,
+            13 : 144447.644200,
+            12 : 288895.288400,
+            11 : 577790.576700,
+            10 : 1155581.153000,
+            9  : 2311162.307000,
+            8  : 4622324.614000,
+            7  : 9244649.227000,
+            6  : 18489298.450000,
+            5  : 36978596.910000,
+            4  : 73957193.820000,
+            3  : 147914387.600000,
+            2  : 295828775.300000,
+            1  : 591657550.500000,
+        },
         initMap = function () {
             map = new google.maps.Map(document.getElementById('map'), {
                 center: {lat: -34.397, lng: 150.644},
                 zoom: 18
             });
+
+            google.maps.event.addListener(map,"center_changed", function(){
+                    var pos = map.getCenter();
+                    Instagram(pos.lat(), pos.lng(),map.getZoom());
+            });
         },
-        Instagram = function (lat, lng) {
+        Instagram = function (lat, lng, zoom) {
             var access_token = "16384709.6ac06b4.49b97800d7fd4ac799a2c889f50f2587",
                 locationIds = [],
                 photoData = [];
@@ -16,8 +44,9 @@
                 type: "GET",
                 dataType: "jsonp",
                 cache: false,
-                url: "https://api.instagram.com/v1/locations/search?lat=" + lat + "&lng=" + lng + "&access_token=" + access_token,
+                url: "https://api.instagram.com/v1/locations/search?lat=" + lat + "&lng=" + lng + "&distance=" + zoomRatios[zoom] + "&access_token=" + access_token,
                 success: function (data) {
+                    console.log(data);
                     for (var obj in data.data) {
                         locationIds.push(data.data[obj]);
                     }
@@ -48,6 +77,11 @@
                                             url: photoData[i].images.thumbnail.url
                                         }
                                     });
+                                    //ensure we don't have too many markers
+                                    markers.push(marker);
+                                    if(markers.length>MAX_MARKERS){
+                                         markers[0].setMap(null);                                                              markers.splice(0,1);
+                                    }
                                 }
                             }
                         });
@@ -65,7 +99,7 @@
                         lng: position.coords.longitude
                     };
                     map.setCenter(pos);
-                    Instagram(pos.lat, pos.lng);
+                    Instagram(pos.lat, pos.lng,map.getZoom());
                 });
             }
         };
